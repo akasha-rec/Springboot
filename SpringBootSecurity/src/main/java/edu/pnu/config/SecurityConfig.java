3fe64e9e9a42b5689186f7b100cfaee6f3d86241
akasha-rec <uspira@naver.com>
2024-05-24 09:35:15 +0900

    JPA 연동 시큐리티

--- SpringBootSecurity/src/main/java/edu/pnu/config/SecurityConfig.java
+++ SpringBootSecurity/src/main/java/edu/pnu/config/SecurityConfig.java
@@ -6,6 +6,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration //설정 클래스라고 정의(loC 컨테이너에 로드)
@@ -33,18 +35,27 @@ public class SecurityConfig {
 				.invalidateHttpSession(true) // 현재 브라우저와 연결된 세션 강제 종료
 				.deleteCookies("JSESSIONID") // 세션 아이디가 저장된 쿠키 삭제
 				.logoutSuccessUrl("/login")); // 로그아웃 후 이동할 URL 지정
+		
+		http.headers(hr->hr.frameOptions(fo->fo.disable())); //url 호출했을 때 프레임만 떠서 추가한 코드 > 정상적으로 화면 뜸
+		 
 		return http.build();
-	}		
+	}
 	
-		@Autowired //임시 테스트용 사용자 계정을 메모리에 등록해서 권한 관계가 제대로 작동하는지 확인 > 코드대로 로그인
-		public void authenticate(AuthenticationManagerBuilder auth) throws Exception {
-			auth.inMemoryAuthentication()
-				.withUser("manager")
-				.password("{noop}abcd") //No Operation > 비밀번호 암호화되어있지 않다
-				.roles("MANAGER");
-			auth.inMemoryAuthentication()
-				.withUser("admin")
-				.password("{noop}abcd")
-				.roles("ADMIN");
-		}
+	@Bean
+	PasswordEncoder passwordEncoder() { //패스워드를 쉽게 암호화할 수 있도록 PasswordEncoder 인터페이스를 구현한 클래스 제공
+		return new BCryptPasswordEncoder();
+	}
+	
+//		JPA 연동 시큐리티 할 때는 없어야 충돌 X	
+//		@Autowired //임시 테스트용 사용자 계정을 메모리에 등록해서 권한 관계가 제대로 작동하는지 확인 > 코드대로 로그인
+//		public void authenticate(AuthenticationManagerBuilder auth) throws Exception {
+//			auth.inMemoryAuthentication()
+//				.withUser("manager")
+//				.password("{noop}abcd") //No Operation > 비밀번호 암호화되어있지 않다
+//				.roles("MANAGER");
+//			auth.inMemoryAuthentication()
+//				.withUser("admin")
+//				.password("{noop}abcd")
+//				.roles("ADMIN");
+//		}
 	}
--- SpringBootSecurity/src/main/java/edu/pnu/controller/LoginController.java
+++ SpringBootSecurity/src/main/java/edu/pnu/controller/LoginController.java
@@ -1,13 +1,25 @@
 package edu.pnu.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import edu.pnu.domain.Member;
+import edu.pnu.domain.Role;
+import edu.pnu.service.MemberService;
 
 @Controller
 public class LoginController {
 	
-	@GetMapping("/login")
-	public void login() {
+	@Autowired private MemberService memberService;
+	
+	@GetMapping("/login") //login.html 호출
+	public void login() { //void > [url명.html] 호출 
 		System.out.println("login 요청");
 	}
 	
@@ -19,5 +31,31 @@ public class LoginController {
 	@GetMapping("/accessDenied")
 	public void accessDenied() {
 		System.out.println("accessDenied");
+	}
+	
+	@GetMapping("/auth")
+	public @ResponseBody ResponseEntity<?> auth(@AuthenticationPrincipal User user) {
+		
+		if(user == null) {
+			return ResponseEntity.ok("로그인 상태가 아닙니다.");
+		}
+		//바꾸고 나서 로그인하고 다시 /auth 호출하니까 해당 정보를 띄워줌
+		return ResponseEntity.ok(user); //user.toString() < 로그인하지 않아서 null인 상태인데 toString해서 에러
+	}
+	
+	@GetMapping("/join")
+	public void join() {}
+	
+	@PostMapping("/join")
+	public String joinProc(Member member) { //String > [return명.html] 호출
+		if (member.getRole() == null) {
+			member.setRole(member.getRole());
+		}
+		
+		member.setRole(Role.ROLE_MEMBER);
+		member.setPassword(member.getPassword());
+		member.setEnabled(true);
+		memberService.save(member);
+		return "welcome"; //welcome.html 호출
 	}
 }
